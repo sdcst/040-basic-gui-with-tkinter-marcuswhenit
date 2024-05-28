db650fe26b54f1f3c54fc2fb966e9e21c4f7dd50
marcuswhenit <159051524+marcuswhenit@users.noreply.github.com>
2024-05-28 12:54:18 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,19 +4,6 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title ("tk")
-window.geometry("500x40")
-
-box1 = tk.Entry(window,text="Entry widgets can be typed in", width=20, relief=SUNKEN, borderwidth=3)
-box2 = tk.Entry(window,text="Entry widgets can be typed in", width=20, relief=SUNKEN, borderwidth=3)
-box3 = tk.Entry(window,text="Entry widgets can be typed in", width=30, relief=SUNKEN, borderwidth=3)
-lable1 = tk.Label(window, text="+", borderwidth=3,)
-lable2 = tk.Label(window, text="=", borderwidth=3,)
-box1.grid(row = 1, column = 1,)
-lable1.grid(row = 1, column = 2)
-box2.grid(row = 1, column = 3)
-lable2.grid(row = 1, column = 4)
-box3.grid(row = 1, column = 5,)
-
-
+window.geometry("600x300")
 
 window.mainloop()
